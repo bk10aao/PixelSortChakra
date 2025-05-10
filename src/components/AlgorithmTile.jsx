08b934e6cbea81b6636202d003dd220aeeea5210
Dr-Psycho-Ben <kane_500@hotmail.com>
2025-05-10 14:48:25 +0100

    fixing lint issues for AlgorithmTile.jsx

--- src/components/AlgorithmTile.jsx
+++ src/components/AlgorithmTile.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import PropTypes from "prop-types";
 import { Box, Text, Button } from "@chakra-ui/react";
 import SortingChart from "./SortingChart";
 import "../algorithmtile.css";
@@ -36,6 +38,18 @@ const AlgorithmTile = ({
       </Button>
     </Box>
   );
+};
+
+AlgorithmTile.propTypes = {
+  algorithm: PropTypes.string.isRequired,
+  steps: PropTypes.arrayOf(PropTypes.any).isRequired,
+  isSorted: PropTypes.bool.isRequired,
+  totalSteps: PropTypes.number.isRequired,
+  onSort: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
+  isSortingAll: PropTypes.bool.isRequired,
+  hasSortedAll: PropTypes.bool.isRequired,
+  height: PropTypes.number.isRequired,
 };
 
 export default AlgorithmTile;
